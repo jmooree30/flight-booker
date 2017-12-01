de45ea761fe951dc0516f734fdd993d6ace54d81
Jacob <jmooree30@gmail.com>
2017-11-30 19:47:18 -0500

    Airport and Flight models migrated

---
+++ db/migrate/20171201004616_create_flights.rb
@@ -0,0 +1,12 @@
+class CreateFlights < ActiveRecord::Migration[5.1]
+  def change
+    create_table :flights do |t|
+      t.integer :departing_airport_id
+      t.integer :arrival_airport_id
+      t.datetime :depature_date
+      t.datetime :arrival_date
+
+      t.timestamps
+    end
+  end
+end
