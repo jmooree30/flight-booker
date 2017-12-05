3d993ef31124165f51c52dc02f572e7f060a8b8f
Jacob <jmooree30@gmail.com>
2017-12-05 00:58:30 -0500

    finishing styling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,6 +8,27 @@ class BookingsController < ApplicationController
 	end 
 
 	def create
-		
+        @flight = Flight.find_by(id: booking_params[:flight_id])
+        @booking = @flight.bookings.build(booking_params)
+        if @booking.save
+        	redirect_to @booking
+          flash[:info] = "Booking successful"
+        else
+          flash[:error] = "Invalid name/email, please try again."
+          redirect_to root_path
+        end 
 	end 
+
+	def show
+      @booking = Booking.find(params[:id])
+      @passengers = @booking.passengers.all
+      @departing = Airport.find(@booking.flight.departing_airport_id)
+      @arriving = Airport.find(@booking.flight.arrival_airport_id)
+	end
+
+	def booking_params
+    params.require(:booking).permit(
+      :flight_id,
+      passengers_attributes:[:id, :name, :email])
+    end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,6 @@
 class Passenger < ApplicationRecord
+	  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+	  validates :name, presence: true, length: { maximum: 20 }
 	  belongs_to :booking, optional: true
       has_one :flight, through: :booking
 end
